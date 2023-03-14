6ec5ab324e02fa6ff6bb8201a05d6cd2aed811e4
Jackson Otto <jacksonotto@Jacksons-MacBook-Pro-2.local>
2023-03-14 10:06:32 -0700

    rename to lampjs

---
+++ lib/src/lampjs.ts
@@ -0,0 +1,115 @@
+import { ComponentAttributes, ComponentChild, ComponentFactory } from 'start-dom-jsx';
+import { isSvgTag, setElementStyle, applyChildren } from 'start-dom-jsx/dist/utils';
+
+export const mount = (root: HTMLElement | null, el: JSX.Element) => {
+  if (!root) return;
+
+  root.appendChild(el);
+};
+
+const getStateEl = <T>(val: T, builder?: (value: T) => JSX.Element) => {
+  if (builder) {
+    return builder(val);
+  } else {
+    const node = document.createElement('span');
+    node.innerHTML = val + '';
+    return node;
+  }
+};
+
+type stateObj<T> = {
+  el: JSX.Element;
+  applyDep: (dep: () => void) => void;
+  value: T;
+};
+
+export const createState = <T>(value: T, builder?: (val: T) => JSX.Element) => {
+  let currentValue = value;
+  let builderCb: ((val: T) => JSX.Element) | undefined = builder;
+  let node: HTMLElement | JSX.Element;
+  let deps: (() => void)[] = [];
+
+  node = getStateEl(currentValue, builderCb);
+
+  const applyDep = (dep: () => void) => {
+    deps.push(dep);
+  };
+
+  return (newValue?: T | ((val: T) => T)) => {
+    if (newValue !== undefined) {
+      if (typeof newValue === 'function') {
+        const updateCb = newValue as (val: T) => T;
+        currentValue = updateCb(currentValue);
+      } else {
+        currentValue = newValue;
+      }
+
+      const newNode = getStateEl(currentValue, builderCb);
+      node.replaceWith(newNode);
+      node = newNode;
+
+      deps.forEach((dep) => {
+        dep();
+      });
+    }
+    return {
+      el: node,
+      value: currentValue,
+      applyDep
+    } as stateObj<T>;
+  };
+};
+
+// export const createEffect = (cb: () => void, deps: ((...args: any[]) => void)[]) => {
+export const createEffect = (cb: () => void, deps: ((val?: any) => stateObj<any>)[]) => {
+  deps.forEach((dep) => {
+    dep().applyDep(cb);
+  });
+};
+
+const xlinkNS = 'http://www.w3.org/1999/xlink';
+export const createElement = (
+  tag: string | ComponentFactory,
+  attrs: ComponentAttributes,
+  ...children: ComponentChild[]
+) => {
+  if (typeof tag === 'function') return tag(Object.assign(Object.assign({}, attrs), { children }));
+  const isSvg = isSvgTag(tag);
+  const element = isSvg
+    ? document.createElementNS('http://www.w3.org/2000/svg', tag)
+    : document.createElement(tag);
+  if (attrs) {
+    if (
+      attrs.style &&
+      typeof attrs.style !== 'string' &&
+      typeof attrs.style !== 'number' &&
+      typeof attrs.style !== 'boolean'
+    ) {
+      setElementStyle(element, attrs.style as Partial<CSSStyleDeclaration>);
+      delete attrs.style;
+    }
+    for (let name of Object.keys(attrs)) {
+      const value = attrs[name];
+      if (name.startsWith('on')) {
+        if (name === 'onChange') {
+          name = 'onInput';
+        }
+        const finalName = name.replace(/Capture$/, '');
+        const useCapture = name !== finalName;
+        const eventName = finalName.toLowerCase().substring(2);
+        if (value && typeof value !== 'string' && typeof value !== 'number' && typeof value !== 'boolean')
+          element.addEventListener(eventName, value as EventListenerOrEventListenerObject, useCapture);
+      } else if (isSvg && name.startsWith('xlink:')) {
+        if (value && typeof value !== 'number' && typeof value !== 'boolean') {
+          element.setAttributeNS(xlinkNS, name, value as string);
+        }
+      } else if (value === true) {
+        element.setAttribute(name, name);
+      } else if (value || value === 0) {
+        element.setAttribute(name, value.toString());
+      }
+    }
+  }
+  applyChildren(element, children);
+  return element;
+};
