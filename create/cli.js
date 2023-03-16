bc26034bbdaa5e322ef233301cfae48b6f9f8cf5
Jackson Otto <jacksonotto@Jacksons-MacBook-Pro-2.local>
2023-03-16 09:05:44 -0700

    working template cli

--- create/cli.js
+++ create/cli.js
@@ -1,29 +1,15 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
-
-const TEMPLATE_DIR = path.join(__dirname, "template");
-
-function copyFiles(source, dest) {
-  const files = fs.readdirSync(source);
-  for (const filename of files) {
-    const isDirectory = fs.lstatSync(path.join(source, filename)).isDirectory();
-    if (isDirectory) {
-      fs.mkdirSync(path.join(dest, filename));
-      copyFiles(path.join(source, filename), path.join(dest, filename));
-    } else {
-      fs.copyFileSync(path.join(source, filename), path.join(dest, filename));
-    }
-  }
-}
 
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question("? What will your project be called: ", (test) => {
-  console.log(test, process.cwd());
+readline.question("? What will your project be called: ", (name) => {
+  const from = path.join(__dirname, "template");
+  const to = path.join(process.cwd(), name);
+  fs.renameSync(path.join(from, "_gitignore"), path.join(from, ".gitignore"));
+  fs.copySync(from, to, { overwrite: true });
   readline.close();
 });
-
-// change
