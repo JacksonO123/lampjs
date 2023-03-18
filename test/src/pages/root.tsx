7d79e33774f8c1b0fa37805042df6ec6f14bd693
Jackson Otto <jacksonotto@Jacksons-MacBook-Pro-2.local>
2023-03-18 10:15:31 -0700

    add name to create template

--- test/src/pages/root.tsx
+++ test/src/pages/root.tsx
@@ -1,25 +1,20 @@
 import { ChangeEvent, createState } from "@jacksonotto/lampjs";
-import Letter from "../components/letter";
 import "./root.css";
 
 const Root = () => {
-  let nums: number[] = [];
   const text = createState("", (val) => {
-    const updateNumsIndex = (val: number, index: number) => {
-      nums[index] = val;
-      console.log(nums);
-    };
+    const num = createState(0);
 
-    console.log(nums);
+    const handleClick = () => {
+      num((prev) => prev + 1);
+    };
 
     return (
       <div>
-        {val.split("").map((c, i) => (
-          <Letter
-            letter={c}
-            startNum={nums[i]}
-            updateNum={(val) => updateNumsIndex(val, i)}
-          />
+        {val.split("").map((c) => (
+          <button onClick={handleClick}>
+            {c} {num().el()}
+          </button>
         ))}
       </div>
     );
@@ -27,11 +22,6 @@ const Root = () => {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.currentTarget.value;
-    if (value.length > nums.length) {
-      nums = [...nums, ...Array(value.length - nums.length).fill(0)];
-    } else if (value.length < nums.length) {
-      nums = nums.slice(0, value.length);
-    }
     text(value);
   };
 
