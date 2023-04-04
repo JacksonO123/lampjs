5d83e47d9a6f95686992e69a034c9ccaffe2165c
Jackson Otto <jacksonotto120@gmail.com>
2023-04-04 14:35:10 -0700

    update create

--- create/template/vite.config.js
+++ create/template/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   esbuild: {
     jsxFactory: "LampJs.createElement",
     jsxFragment: "LampJs.Fragment",
