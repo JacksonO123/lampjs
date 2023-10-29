6f03debc0d47869dec99db24ae841d3f708cd8c7
Jackson Otto <jacksonotto120@gmail.com>
2023-10-28 19:56:45 -0700

    move script tag to body in create template

--- create/template/src/vite-env.d.ts
+++ create/template/src/vite-env.d.ts
@@ -3,6 +3,6 @@ import { createElement, Fragment } from "@jacksonotto/lampjs";
 /// <reference types="vite/client" />
 
 declare global {
-  let createElementLampJs: typeof createElement;
-  let FragmentLampJs: typeof Fragment;
+  let createElement: typeof createElement;
+  let Fragment: typeof Fragment;
 }
