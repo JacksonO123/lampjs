5fa1228873bd694d36a2ffee066ffae6df300ec5
Jackson Otto <jacksonotto120@gmail.com>
2023-11-02 11:17:46 -0700

    (create) v2.3.2: Reorder types in env.d.ts

--- create/template/src/vite-env.d.ts
+++ create/template/src/vite-env.d.ts
@@ -1,6 +1,6 @@
-import { createElement, Fragment } from "@jacksonotto/lampjs";
-
 /// <reference types="vite/client" />
+
+import { createElement, Fragment } from "@jacksonotto/lampjs";
 
 declare global {
   let createElement: typeof createElement;
