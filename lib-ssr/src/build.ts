928fdeaf7b735c40a59df8fcbf930c665b72198c
Jackson Otto <jacksonotto120@gmail.com>
2023-12-17 01:03:05 -0800

    add specific build for vercel deployment, remove lampjs-vercel package

--- lib-ssr/src/build.ts
+++ lib-ssr/src/build.ts
@@ -1,9 +1,35 @@
 import { build } from 'esbuild';
-import { resolve } from 'path';
-import { readFileSync, writeFileSync } from 'fs';
+import { resolve, dirname } from 'path';
 import { outDir } from './constants.js';
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
 
-const path = resolve(process.cwd(), 'src', 'main.tsx');
+const cwd = process.cwd();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const args = process.argv.slice(2);
+const ref = args[1];
+
+if (ref) {
+  const refPath = resolve(__dirname, '../', 'refs', ref + '.json');
+  const exists = existsSync(refPath);
+
+  if (exists) {
+    const data = JSON.parse(readFileSync(refPath, 'utf-8'));
+    const path = resolve(cwd, data.path);
+
+    const dirPath = dirname(path);
+    const dirExists = existsSync(dirPath);
+
+    if (!dirExists) {
+      mkdirSync(dirPath);
+    }
+
+    writeFileSync(path, data.code);
+  }
+}
+
+const path = resolve(cwd, 'src', 'main.tsx');
 
 await build({
   jsxFactory: 'createElementSSR',
@@ -20,11 +46,15 @@ await build({
   external: ['@jacksonotto/lampjs-ssr']
 });
 
-const createElementFn = `import { createElementSSR } from '@jacksonotto/lampjs-ssr';
-const createElement = createElementSSR;\n`;
+const createElementFn = `import { createElementSSR } from '@jacksonotto/lampjs-ssr';const createElement = createElementSSR;`;
 
-const codePath = resolve(process.cwd(), outDir, 'main.js');
+const codePath = resolve(cwd, outDir, 'main.js');
 let code = readFileSync(codePath, 'utf-8');
 code = createElementFn + code;
-
 writeFileSync(codePath, code);
+
+const child = spawn('npx', ['rollup', 'dist-ssr/main.js', '-o', 'dist-ssr/main.js']);
+
+child.stderr.on('data', (data) => {
+  console.log(data + '');
+});
--- lib-vercel/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(request: VercelRequest, response: VercelResponse): Promise<void>;
