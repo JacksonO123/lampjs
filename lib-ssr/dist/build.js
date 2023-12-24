e2a07f2551eab750da592a3bed324f8ec924796c
Jackson Otto <jacksonotto120@gmail.com>
2023-12-24 02:36:47 -0800

    add server refs to ssr lib

--- lib-ssr/dist/build.js
+++ lib-ssr/dist/build.js
@@ -1,27 +1,9 @@
 import { build } from 'esbuild';
-import { resolve, dirname } from 'path';
+import { resolve } from 'path';
 import { outDir } from './constants.js';
-import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { spawn } from 'child_process';
-import { fileURLToPath } from 'url';
 const cwd = process.cwd();
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const args = process.argv.slice(2);
-const ref = args[1];
-if (ref) {
-    const refPath = resolve(__dirname, '../', 'refs', ref + '.json');
-    const exists = existsSync(refPath);
-    if (exists) {
-        const data = JSON.parse(readFileSync(refPath, 'utf-8'));
-        const path = resolve(cwd, data.path);
-        const dirPath = dirname(path);
-        const dirExists = existsSync(dirPath);
-        if (!dirExists) {
-            mkdirSync(dirPath);
-        }
-        writeFileSync(path, data.code);
-    }
-}
 const path = resolve(cwd, 'src', 'main.tsx');
 await build({
     jsxFactory: 'createElementSSR',
---
+++ lib-ssr/dist/refs/vercel.d.ts
@@ -0,0 +1,2 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+export default function handler(request: VercelRequest, response: VercelResponse): Promise<void>;
